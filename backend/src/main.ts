2f0b59090d01464b869939c98e3903c1c1c9aabb
harkanni <harkanzyempire001@gmail.com>
2026-09-10 20:54:57 +0100

    Refactor CORS configuration to support multiple origins

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,10 +5,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173', // your Vite dev server
+    origin: [
+      'http://localhost:5173', // Your Vite dev server
+      'https://hurdle-frontend-ewnp.onrender.com', // Your hosted Render website
+    ],
     credentials: true,
   });
-  
+
   await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
